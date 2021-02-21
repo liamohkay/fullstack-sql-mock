f218b933cd4d61fb4eeba43cf65cbe3f43cab2c1
liamohkay <liam.olsonkenny@gmail.com>
2021-02-21 13:38:15 -0800

    Wrote out helpers for GET & POST methods

--- db/dbhelpers.js
+++ db/dbhelpers.js
@@ -1,11 +1,35 @@
-// complete and fix the dbhelpers
+const db = require('./index.js');
 
+var getQueryArgs = req => {
+  let cols = [];
+  let vals = [];
 
-getProductsHelper = () => 
+  Object.keys(req.body).map(key => {
+    cols.push(key);
+    vals.push(`'${product[key]}'`);
+  });
 
-postProductsHelper = () => 
+  return { cols, vals };
+};
 
-updateProductHelper = () => 
+const dbHelpers = {
 
-deleteProductHelper = () => 
+  getProductsHelper: (callback) => {
+    let query = `SELECT * FROM products`;
+    db.query(query, (err, data) => callback(err, data));
+  },
+
+  postProductsHelper: (req, callback) => {
+    let args = getQueryArgs(req);
+    let query = `
+      INSERT INTO products (${args.cols.join(', ')})
+      VALUES (${args.vals.join(', ')})`;
+    db.query(query, (err, data) => callback(err, data));
+  }
+
+  // updateProductHelper = () =>
+
+  // deleteProductHelper = () =>
+
+}
 
--- server/index.js
+++ server/index.js
@@ -5,21 +5,21 @@
   const router = require('./router');
 
   const server = express();
-  
+
   server.use('/', express.static(path.join(__dirname + '/../client/dist')));
-  
+
   server.get('/name', (req, res) => {
     res.status(200).send('This is your get request, modify this file to use your router!')
   })
-  
+
   server.post('/name', (req, res) => {
     res.status(200).send('This is your post request, modify this file to use your router!')
   })
-  
+
   server.put('/name', (req, res) => {
     res.status(200).send('This is your put request, modify this file to use your router!')
   })
-  
+
   server.delete('/name', (req, res) => {
     res.status(200).send('This is your delete request, modify this file to use your router!')
   })
