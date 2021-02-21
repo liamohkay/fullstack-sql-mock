cf3156bd6eccce70ac6911b9e30a0e30af93d92f
liamohkay <liam.olsonkenny@gmail.com>
2021-02-21 13:23:29 -0800

    Fixed seed file, added script to run, added items to db

--- db/seed.js
+++ db/seed.js
@@ -1,9 +1,9 @@
 
 // const Products = require('./models.js');
-// const db = require('./index.js');
+const db = require('./index.js');
 
-// Fill in the definition of insertMockData so that when 
-// this file is run in the terminal with `node seed.js`, 
+// Fill in the definition of insertMockData so that when
+// this file is run in the terminal with `node seed.js`,
 // all 10 products are inserted into the database
 
 const adjectives = ['Used', 'New', 'Refurbished', "PARTS ONLY"];
@@ -16,7 +16,7 @@ const createProduct = () => {
   product.min_cost = parseFloat(Math.ceil(Math.random() * Math.ceil(1000)));
   product.curr_bid = parseFloat(Math.ceil(Math.random() * Math.ceil(10000)));
   product.ends_in = Math.ceil(Math.random() * Math.ceil(3));
-  // the lorempixel images render very slowly for some reason. 
+  // the lorempixel images render very slowly for some reason.
   // don't worry too much if some images load while the others don't.
   // it's probably not your fault
   product.image = `http://lorempixel.com/400/400/technics/${Math.ceil(Math.random() * Math.ceil(10))}`;
@@ -32,9 +32,23 @@ const createProducts = () => {
 }
 
 const insertMockData = function() {
-  // Complete me please
-};
+  let mockProducts = createProducts();
 
-// NOTE: DO NOT invoke this function as part of your
-// server code - it is meant to only be run once so that
-// you have access to data to work with
+  mockProducts.map(product => {
+    let cols = [];
+    let vals = [];
+
+    Object.keys(product).map(key => {
+      cols.push(key);
+      vals.push(`'${product[key]}'`);
+    });
+
+    let query = `
+      INSERT INTO products (${cols.join(', ')})
+      VALUES (${vals.join(', ')});`
+    db.query(query, (err, result) => {
+      if (err) console.log(err);
+      else console.log(`Successfully added ${product.item} to db`);
+    });
+  });
+}();
