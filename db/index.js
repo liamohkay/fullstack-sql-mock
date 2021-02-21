f62a2e689a8cb4f94f236e9f58898f2adc42eea5
liamohkay <liam.olsonkenny@gmail.com>
2021-02-21 12:55:32 -0800

    Created db connection

--- db/index.js
+++ db/index.js
@@ -1,1 +1,12 @@
-// build your database
+const mysql = require('mysql');
+const db = 'ebay';
+
+const connection = mysql.createConnection({
+  user: 'root',
+  password: 'password',
+  database: db
+});
+
+connection.connect(() => console.log(`SUCCESS: Connected to ${db} database`));
+
+module.exports = connection;
